7d66932defe49cd0a92378c2149905d8efd75e1c
chen bc <chenbc@dragoninfo.com.cn>
2023-10-20 16:55:04 +0800

    添加批量查询接口

--- src/main/java/cbc/boot/myboot/controller/db/Select.java
+++ src/main/java/cbc/boot/myboot/controller/db/Select.java
@@ -71,6 +71,65 @@ public class Select {
             return returnList;
         }
     }
+    @PostMapping("/cbc/selectByBatch.cbc")
+    public List<Object>  selectByBatch(@RequestBody String param, HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("----------Start(Author:陈斌才)----------");
+        System.out.println("执行select操作!");
+        System.out.println("传入参数:");
+        System.out.println(param);
+        List<Object> returnList = new ArrayList<Object>();
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        SqlSession sqlSession=null;
+        Date date1=new Date();
+        String realIP = GetIPUtil.getIpAddr(request);
+        System.out.println("请求客户端IP地址：" + realIP);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//可以方便地修改日期格式
+        String nowTime = dateFormat.format(date1);
+        System.out.println("请求时间：" + nowTime);
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map map=mapper.readValue(param, Map.class);
+            //设置数据源
+            String db=(String) map.get("db");
+            if(db!=null && !"".equals(db)){
+                DynamicDataSourceHolder.setDataSourceType(db);
+            }
+            sqlSession=sqlSessionFactory.openSession();
+
+            List<Map<String, Object>>  batchSqlList=(ArrayList) map.get("batchSql");
+            System.out.println("请求参数1：" + (ArrayList) map.get("batchSql"));
+
+            for(int i=0;i<batchSqlList.size();i++){
+                String sql=(String) batchSqlList.get(i).get("sql");
+                String resultKey=(String) batchSqlList.get(i).get("resultKey");
+                List<Map<String, Object>> list=sqlSession.selectList(sql, batchSqlList.get(i));
+                hashMap.put(resultKey, list);
+            }
+
+
+            //System.out.println("查询到的记录数："+list.size());
+            hashMap.put("state", "success");
+            Date date2=new Date();
+            String durationTime=(date2.getTime() - date1.getTime()) + "MS";
+            hashMap.put("time", durationTime);
+            System.out.println("执行时间："+durationTime);
+        }catch (Exception e) {
+            e.printStackTrace();
+            hashMap.put("state", "error");
+            hashMap.put("message", e.getMessage());
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+                sqlSession = null;
+            }
+            returnList.add(hashMap);
+            System.out.println("----------End(Author:陈斌才)----------");
+            return returnList;
+        }
+
+
+    }
 
 
 }
